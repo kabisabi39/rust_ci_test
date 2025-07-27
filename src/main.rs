3d4522c1eb9ef816f525e6cfbca5081c72c4e941
kabisabi39 <kabisabi39@gmail.com>
2025-07-27 12:28:23 +0900

    Modify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn even_or_odd(number: i32) -> String {
+    if number % 2 == 0 {
+        "even".to_string()
+    } else {
+        "odd".to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", even_or_odd(10));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_even_or_odd() {
+        assert_eq!(even_or_odd(10), "even");
+    }
 }
